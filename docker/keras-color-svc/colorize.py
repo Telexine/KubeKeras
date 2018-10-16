6050032d5d67a083907e99f25e90d3f2742c7c7e
Tachid Boonpipat <tanuki360@hotmail.com>
2018-10-16 16:05:53 +0700

    Implement upload Image across services

--- docker/keras-color-svc/colorize.py
+++ docker/keras-color-svc/colorize.py
@@ -1,8 +1,8 @@
-from flask import Flask,request
-
+from flask import Flask,request,url_for, send_from_directory
+from werkzeug.datastructures import ImmutableMultiDict
 import os
 app = Flask(__name__)
-
+from werkzeug import secure_filename
 
 
 ## Keras
@@ -16,22 +16,33 @@ import numpy as np
 from keras_contrib.layers.normalization import InstanceNormalization
 
 
+app.config['upload'] = "upload"
+
 
 @app.route('/')
 def hello():
     return 'Hello Container World!'
 
-@app.route('/gen',methods =  ['GET','POST'])
+@app.route('/gen',methods =  ['POST'])
 def gen():
+    print("hello")
     if request.method == 'POST':
-        """modify/update the information for <user_id>"""
-        # you can use <user_id>, which is a str but could
-        # changed to be int or whatever you want, along
-        # with your lxml knowledge to make the required
-        # changes
-        data = request.form # a multidict containing POST data
-    
-        return 'Hello Container World gen!'
+        file = request.files['file']
+        if file :
+            print '**found file', file.filename
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['upload'], filename))
+            # for browser, add 'redirect' function on top of 'url_for'
+            print(url_for('uploaded_file',
+                                    filename=filename))
+            return url_for('uploaded_file',
+                                    filename=filename)
 
+        return 'Error'
+
+@app.route('/upload/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['upload'],
+                               filename)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
--- docker/node/server.js
+++ docker/node/server.js
@@ -2,33 +2,88 @@
 var request = require('request');
 const express = require('express');
 var path = require('path'),
+fs = require('fs'),
 bodyParser = require('body-parser')
+var multer  = require('multer')
 // Constants
 const PORT = 8888;
 const HOST = '0.0.0.0';
-
-
 // App
 const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use('/static',express.static('static'));
+app.use('/node_modules',express.static('node_modules'));
+app.use('/css',express.static('node_modules/materialize-css/dist/css'));
+app.use('/js',express.static('node_modules/materialize-css/dist/js'));
 app.get('/', (req, res) => {
-  res.send('Hello world v3\n');
+  res.sendFile(path.join(__dirname+'/static/index.html'));
 });
 
-app.use(express.static('public'));
-app.use(bodyParser.json());
+var upload = multer({ dest: 'uploads/' })
+
+
+app.post('/colorize',upload.single('image') ,(req, res,next) => {
  
 
-app.post('/colorize', (req, res) => {
-  let data = {
-    api_key: "DD"
-  };
-  request.post("http://0.0.0.0:5000/gen"/*,{formData:data}*/, function optionalCallback(err, httpResponse, body) {
-    if (err) {
-      return console.error('analize failed:', err);
+
+  //console.log(req.file)
+  /** RESPONSE
+   
+  { fieldname: 'image',
+  originalname: 'f1920x960.jpeg',
+  encoding: '7bit',
+  mimetype: 'image/jpeg',
+  destination: 'uploads/',
+  filename: '0d13a5aa9acad5994a6957d9b7649996',
+  path: 'uploads/0d13a5aa9acad5994a6957d9b7649996',
+  size: 386996 }
+
+
+  const options = {
+    method: "POST",
+    url: "http://127.0.0.1:5000/gen",
+    port: 5000,
+    headers: {
+        "Content-Type": "image/jpeg"
+    },
+    formData : {
+        "image" : fs.createReadStream(req.file.path)
     }
-    var result =  body;
-    console.log(result)
-    res.send(result)
+  };
+ 
+  request(options, function (err, res, body) {
+    if(err) console.log(err);
+    console.log(body);
+  });
+   */
+  let newfn = req.file.destination+req.file.filename+"."+req.file.mimetype.substr(req.file.mimetype.indexOf("/")+1,req.file.mimetype.length);
+  console.log(newfn)
+  fs.rename(req.file.path,newfn, function (err) {
+    if (err) throw err;
+    fs.stat(newfn, function (err, stats) {
+      if (err) throw err;
+      console.log('stats: ' + JSON.stringify(stats));
+
+
+  var formData = {
+    file: fs.createReadStream(newfn)
+  };
+  
+  request.post({url:'http://127.0.0.1:5000/gen', formData: formData}, function(err, httpResponse, body) {
+    if (err) {
+      return console.error('upload failed:', err);
+    }
+    console.log('Upload successful!  Server responded with:', body);
+  });
+  res.status(200).send("ok");
+  /*
+
+  });
+
+  */
+  });
   });
 });
 
